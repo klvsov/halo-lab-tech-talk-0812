b3e3d47289415e7aa40864211383ca2b65095fd6
klvsov <klvsov@gmail.com>
2021-12-09 20:51:31 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { usersApi } from './redux/usersApi';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApiProvider api={usersApi}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ApiProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
